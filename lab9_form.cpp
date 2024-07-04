65cb715b41cb5f7b45aef18b3dbd53f4e93a88a1
katrinevo <katrinevo.lap@gmail.com>
2024-07-04 19:41:01 +0300

    9 lab, formating

---
+++ lab9_form.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include <format>
+
+
+int main() {
+
+    std::cout << std::format("{0} {0} {2}: {3}!\n", "Hello", "goodbye", "world", 2024);
+    std::cout << std::format("{:+<7}", 'y') << '\n'; 
+    std::cout << std::format("{:=^5}", 'x') << '\n';
+    std::cout << std::format("{:>9}", false) << '\n';
+    std::cout << "\n";
+    std::cout << std::format("{0:+},{0:-},{0: }", 5) << '\n';
+    std::cout << std::format("{0:+},{0:-},{0: }", -7) << '\n';
+    std::cout << "\n";
+    std::cout << std::format("{:g}", 120.00) << '\n'; 
+    std::cout << std::format("{:#g}", 120.48345) << '\n'; //три числа после запятой
+  
+
+    return 0;
+}
